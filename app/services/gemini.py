a9b15bb7be83a696db5d20acb5bf6f16fd270a8c
Alyas <alyasmuhd1234@gmail.com>
2025-07-17 18:24:33 +0800

    feat: update to incldue gemini lib, and test the prompt using client code

---
+++ app/services/gemini.py
@@ -0,0 +1,15 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+gemini_key=os.getenv("GEMINI_API_KEY")
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client(api_key=gemini_key)
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how car works in a few words",
+)
+print(response.text)
